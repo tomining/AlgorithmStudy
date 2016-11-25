c02eaa08b833ab53227fa52884a39e5eb87bd2ae
jeongwoo.cho <jeongwoo.cho@nhn.com>
2016-11-25 11:05:43 +0900

    initial commit for N-Queen problme

---
+++ src/com/tomining/backtracking/NQueenSolution.java
@@ -0,0 +1,11 @@
+package com.tomining.backtracking;
+
+/**
+ * Question: N-Queen problem
+ * 가로 세로 모두 N개의 칸이 있는 체스판 위에 N개의 여왕을 올려놓되 서로 공격해서
+ * 잡을 수 없도록 놓을 수 있는 방법은 모두 몇 개인가?
+ *
+ * @author tomining
+ */
+public class NQueenSolution {
+}
